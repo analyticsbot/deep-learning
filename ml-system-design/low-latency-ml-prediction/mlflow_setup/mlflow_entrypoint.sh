2a76fdba23606bd6732430ffd64864569453e5e1
Ravi <ravi.shankar1788@gmail.com>
2024-11-02 22:23:53 -0700

    use case for ML System Design Harmful Content Detection System

--- ml-system-design/low-latency-ml-prediction/mlflow_setup/mlflow_entrypoint.sh
+++ ml-system-design/low-latency-ml-prediction/mlflow_setup/mlflow_entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-mkdir -p ./mlruns
-
-# Set permissions to allow write access
-chmod -R 777 ./mlruns
+sudo chmod 755 -R /mlflow_setup/mlruns
 
 # Start MLflow server
 mlflow server \
   --backend-store-uri sqlite:///mlflow.db \
-  --default-artifact-root ./mlruns \
+  --default-artifact-root /mlflow_setup/mlruns \
   --host 0.0.0.0 \
-  --port 5000
+  --port 5000 \
+  --file-store /mlflow_setup/mlruns/
